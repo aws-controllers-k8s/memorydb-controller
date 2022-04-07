e8b6c1cb3e4c172668a446ea613a32170d11ea8b
Kumar Gaurav Sharma <kumargsh@amazon.com>
2022-04-06 22:14:27 -0700

    Regenerate controller code to fix missing aws_fieldexports.yaml entry in kustomization yaml

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -18,6 +18,7 @@ package main
 import (
 	"os"
 
+	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 	ackcfg "github.com/aws-controllers-k8s/runtime/pkg/config"
 	ackrt "github.com/aws-controllers-k8s/runtime/pkg/runtime"
 	ackrtutil "github.com/aws-controllers-k8s/runtime/pkg/util"
@@ -31,7 +32,6 @@ import (
 
 	svctypes "github.com/aws-controllers-k8s/memorydb-controller/apis/v1alpha1"
 	svcresource "github.com/aws-controllers-k8s/memorydb-controller/pkg/resource"
-	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 
 	_ "github.com/aws-controllers-k8s/memorydb-controller/pkg/resource/acl"
 	_ "github.com/aws-controllers-k8s/memorydb-controller/pkg/resource/cluster"
@@ -39,6 +39,8 @@ import (
 	_ "github.com/aws-controllers-k8s/memorydb-controller/pkg/resource/snapshot"
 	_ "github.com/aws-controllers-k8s/memorydb-controller/pkg/resource/subnet_group"
 	_ "github.com/aws-controllers-k8s/memorydb-controller/pkg/resource/user"
+
+	"github.com/aws-controllers-k8s/memorydb-controller/pkg/version"
 )
 
 var (
@@ -104,7 +106,11 @@ func main() {
 	)
 	sc := ackrt.NewServiceController(
 		awsServiceAlias, awsServiceAPIGroup, awsServiceEndpointsID,
-		ackrt.VersionInfo{}, // TODO: populate version info
+		ackrt.VersionInfo{
+			version.GitCommit,
+			version.GitVersion,
+			version.BuildDate,
+		},
 	).WithLogger(
 		ctrlrt.Log,
 	).WithResourceManagerFactories(
