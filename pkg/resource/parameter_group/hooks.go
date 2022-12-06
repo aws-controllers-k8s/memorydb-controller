9782cabd9c551a84e75e34e7497843fea3f47316
Kyrie Chen <kyriezch@amazon.com>
2022-12-05 20:23:21 -0500

    Merges hooks into one file.

--- pkg/resource/parameter_group/hooks.go
+++ pkg/resource/parameter_group/hooks.go
@@ -15,10 +15,87 @@ package parameter_group
 
 import (
 	"context"
-
 	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
+
+	svcapitypes "github.com/aws-controllers-k8s/memorydb-controller/apis/v1alpha1"
+	ackerr "github.com/aws-controllers-k8s/runtime/pkg/errors"
 	svcsdk "github.com/aws/aws-sdk-go/service/memorydb"
 )
+
+func (rm *resourceManager) setParameters(
+	ctx context.Context,
+	ko *svcapitypes.ParameterGroup,
+) (*svcapitypes.ParameterGroup, error) {
+	if ko.Spec.Name == nil {
+		return ko, nil
+	}
+
+	parameters, err := rm.describeParameters(ctx, ko.Spec.Name)
+	if err != nil {
+		return nil, err
+	}
+	parameterNameValues := []*svcapitypes.ParameterNameValue{}
+	for _, p := range parameters {
+		sp := svcapitypes.ParameterNameValue{
+			ParameterName:  p.Name,
+			ParameterValue: p.Value,
+		}
+		parameterNameValues = append(parameterNameValues, &sp)
+	}
+	ko.Spec.ParameterNameValues = parameterNameValues
+	// Update parameter value to nil to not duplicate status and spec values.
+	for _, p := range parameters {
+		p.Value = nil
+	}
+	ko.Status.Parameters = parameters
+	return ko, nil
+}
+
+func (rm *resourceManager) describeParameters(
+	ctx context.Context,
+	parameterGroupName *string,
+) ([]*svcapitypes.Parameter, error) {
+
+	parameters := []*svcapitypes.Parameter{}
+	var nextToken *string = nil
+	for {
+		response, respErr := rm.sdkapi.DescribeParametersWithContext(ctx, &svcsdk.DescribeParametersInput{
+			ParameterGroupName: parameterGroupName,
+			NextToken:          nextToken,
+		})
+		rm.metrics.RecordAPICall("GET", "DescribeParameters", respErr)
+		if respErr != nil {
+			if awsErr, ok := ackerr.AWSError(respErr); ok && awsErr.Code() == "ParameterGroupNotFound" {
+				return nil, ackerr.NotFound
+			}
+			return nil, respErr
+		}
+		if response.Parameters == nil {
+			break
+		}
+
+		// Update the next token
+		nextToken = response.NextToken
+
+		for _, p := range response.Parameters {
+			sp := svcapitypes.Parameter{
+				Name:                 p.Name,
+				Value:                p.Value,
+				Description:          p.Description,
+				DataType:             p.DataType,
+				AllowedValues:        p.AllowedValues,
+				MinimumEngineVersion: p.MinimumEngineVersion,
+			}
+			parameters = append(parameters, &sp)
+		}
+
+		if nextToken == nil || *nextToken == "" {
+			break
+		}
+	}
+
+	return parameters, nil
+}
 
 func (rm *resourceManager) resetParameterGroup(
 	ctx context.Context,
